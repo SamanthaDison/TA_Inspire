177686d7253c0d9c908e6d14b52105937ff9eaaf
SamanthaDison <samnicdis@gmail.com>
2022-04-12 10:50:01 -0600

    working on create todo method

--- app/Controllers/TodosController.js
+++ app/Controllers/TodosController.js
@@ -1,6 +1,7 @@
 import { todosService } from "../Services/TodosService.js";
 import { ProxyState } from "../AppState.js";
 
+// TODO for draw todos function... need to check if array.length >0 THEN run draw...using same id
 export class TodosController {
     constructor() {
         console.log('todos controller');
@@ -8,7 +9,19 @@ export class TodosController {
 
     showTodos() {
         console.log('show todos')
-        let todos = document.getElementById('todos')
-        todos.classList.toggle("todos-display")
+        let todos = document.getElementById('todo-row')
+        todos.classList.toggle("display-none")
+    }
+
+    showTodoForm() {
+        console.log('todo form')
+        document.getElementById('todo-form').style.display = "block";
+        document.getElementById('todo-button').style.display = "none";
+    }
+
+    createTodo() {
+        window.event.preventDefault()
+        let form = window.event.target
+
     }
 }
